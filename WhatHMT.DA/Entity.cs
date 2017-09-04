38ffd9830e817a8feef2c19bf3726a1925e6e80f
chungontsui <chungont@gmail.com>
2017-09-04 17:46:58 +1200

    Started DA classes

---
+++ WhatHMT.DA/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatHMT.DA
+{
+	public class StoreToy
+	{
+		public Guid StoreGUID { get; set; }
+		public Guid ToyGUID { get; set; }
+		public string LastUpdatedBy { get; set; }
+
+	}
+}
---
+++ WhatHMT.DA/iDA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatHMT.DA
+{
+	public interface iDA
+	{
+		void UpdateStoreToyStatus();
+		void AddNewStoreToy();
+	}
+}
