af27bb8d50c8c11176491be4997935021aeee1e3
chungontsui <chungont@gmail.com>
2017-09-06 17:47:32 +1200

    Added Methods to DA

--- WhatHMT.DA/DA.cs
+++ WhatHMT.DA/DA.cs
@@ -6,17 +6,67 @@ using System.Threading.Tasks;
 
 namespace WhatHMT.DA
 {
-	public class DA : iRepo
+	public class DA
 	{
-		public void AddNewStoreToy()
+		public List<StoreToys> getStoreToysByStoreGuid(Guid StoreGuid)
 		{
-			throw new NotImplementedException();
+			List<StoreToys> _result = new List<StoreToys>();
+			try
+			{
+				using (var _context = new Context())
+				{
+					var st = _context.dsStoreToys;
+
+					if (StoreGuid != Guid.Empty)
+					{
+						st.Where(t => t.StoreGUID == StoreGuid);
+					}
+
+					_result = st.ToList();
+				}
+
+				return _result;
+			}
+			catch (Exception e)
+			{
+				throw new Exception("Fatal: getStoreToysByStoreGuid " + e.Message, e.InnerException);
+			}
 		}
 
-		public void UpdateStoreToyStatus()
+		public StoreToys addNewStoreToy(StoreToys newStoreToy)
 		{
-			throw new NotImplementedException();
+			try
+			{
+				using (var _context = new Context())
+				{
+					_context.dsStoreToys.Add(newStoreToy);
+					_context.SaveChanges();
+				}
+
+				return newStoreToy;
+			}
+			catch (Exception e)
+			{
+				throw new Exception("Fatal: addNewStoreToys " + e.Message, e.InnerException);
+			}
+
 		}
 
+		public void updateStoreToys(StoreToys updateStoreToy)
+		{
+			try
+			{
+				using (var _context = new Context())
+				{
+					var _storeToy = _context.dsStoreToys.First(s => s.Id == updateStoreToy.Id);
+
+				}
+
+			}
+			catch (Exception e)
+			{
+				throw new Exception("Fatal: addNewStoreToys " + e.Message, e.InnerException);
+			}
+		}
 	}
 }
